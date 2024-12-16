7bdafb6ad9ce416ae51de085fc1da2f8a54617a5
Troya A <aetroyab@uce.edu.ec>
2024-12-15 20:09:52 -0500

    Payment new

---
+++ src/main/java/ec/edu/uce/payments/HelloResource.java
@@ -0,0 +1,14 @@
+package ec.edu.uce.payments;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
